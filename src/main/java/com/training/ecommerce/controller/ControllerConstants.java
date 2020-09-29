ebdd90a3b7a38e7826f63b38c1eefb03f9cf0bec
Mateo <matteohenao36@gmail.com>
2020-09-28 22:21:42 -0500

    (TRA-3) - Refactoring code

--- src/main/java/com/training/ecommerce/controller/ControllerConstants.java
+++ src/main/java/com/training/ecommerce/controller/ControllerConstants.java
@@ -3,11 +3,11 @@ package com.training.ecommerce.controller;
 public interface ControllerConstants {
 
     interface User{
-        public static final String ROOT_REQUEST = "/user";
-        public static final String CREATE = "/create";
-        public static final String UPDATE = "/update";
-        public static final String FIND_BY_EMAIL = "/find";
-        public static final String FIND_ALL_USERS = "/findAllUsers";
-        public static final String REQUEST_ORIGIN = "http://localhost:4200";
+        String ROOT_REQUEST = "/user";
+        String CREATE = "/create";
+        String UPDATE = "/update";
+        String FIND_BY_EMAIL = "/find";
+        String FIND_ALL_USERS = "/findAllUsers";
+        String REQUEST_ORIGIN = "http://localhost:4200";
     }
 }
