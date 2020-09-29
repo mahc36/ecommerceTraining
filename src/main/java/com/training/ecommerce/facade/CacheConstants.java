810a8762ed5e96c72083660439abee22bbfa5ee8
Mateo <matteohenao36@gmail.com>
2020-09-28 20:26:27 -0500

    (TRA-3) - Implementing cache for users

---
+++ src/main/java/com/training/ecommerce/facade/CacheConstants.java
@@ -0,0 +1,9 @@
+package com.training.ecommerce.facade;
+
+public interface CacheConstants {
+
+    interface User {
+        String FIND_ALL_USERS_CACHE =  "findAllUsersCache";
+    }
+
+}
--- src/main/java/com/training/ecommerce/facade/user/impl/DefaultUserFacade.java
+++ src/main/java/com/training/ecommerce/facade/user/impl/DefaultUserFacade.java
@@ -2,11 +2,15 @@ package com.training.ecommerce.facade.user.impl;
 
 import com.training.ecommerce.data.UserData;
 import com.training.ecommerce.exception.DuplicatedUserException;
+import com.training.ecommerce.facade.CacheConstants;
 import com.training.ecommerce.facade.Converter;
 import com.training.ecommerce.facade.user.UserFacade;
 import com.training.ecommerce.model.UserModel;
 import com.training.ecommerce.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.annotation.CacheEvict;
+import org.springframework.cache.annotation.Cacheable;
+import org.springframework.cache.annotation.Caching;
 import org.springframework.stereotype.Service;
 import org.springframework.util.Assert;
 
@@ -23,6 +27,9 @@ public class DefaultUserFacade implements UserFacade {
     private Converter<UserData, UserModel> userConverter;
 
     @Override
+    @Caching(evict = {
+        @CacheEvict(value = CacheConstants.User.FIND_ALL_USERS_CACHE, allEntries = true)
+    })
     public UserData createUser(final UserData user) throws DuplicatedUserException {
         Assert.notNull(user, user.getClass() + " cannot be empty");
 
@@ -58,6 +65,7 @@ public class DefaultUserFacade implements UserFacade {
     }
 
     @Override
+    @Cacheable(cacheNames = CacheConstants.User.FIND_ALL_USERS_CACHE)
     public List<UserData> findAllUsers() {
         return getUserConverter().convertAll2Data(getUserService().findAllUsers());
     }
